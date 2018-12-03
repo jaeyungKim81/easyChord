c7adb50cb7e8ce1aa47266c5b95af7dceb2615b3
mg.kim <mg.kim@matetek.com>
2018-12-03 14:31:14 +0900

    Initial Commit

---
+++ easyChord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  easyChord
+//
+//  Created by USER on 2018. 12. 3..
+//  Copyright © 2018년 USER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
